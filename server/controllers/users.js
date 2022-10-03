c35a191f7ad3d51e33aaab438072fe1a325ace59
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-10-02 21:28:17 -0500

    Added updateUser controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -60,6 +60,13 @@ export const loginUser = async (req, res, next) => {
     }
 };
 
-export const updateUser = async (req, res, next) => { res.json("Authenticated user") };
+export const updateUser = async (req, res, next) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        next(err);
+    }
+};
 
 export const deleteUser = async (req, res, next) => { };
