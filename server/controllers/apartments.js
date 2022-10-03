b10a0b67d5de162ab896197c1e2575e77fc62552
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-10-02 23:10:51 -0500

    Added conditional for if no building is found in create apartment controller, added comments

--- server/controllers/apartments.js
+++ server/controllers/apartments.js
@@ -1,6 +1,7 @@
 import Building from "../models/Building.js";
 import Apartment from "../models/Apartment.js";
 
+// Get all apartments, admin only route!
 export const getAllApartments = async (req, res, next) => {
     try {
         const allApartments = await Apartment.find({});
@@ -11,6 +12,7 @@ export const getAllApartments = async (req, res, next) => {
 };
 export const getApartment = async (req, res, next) => { };
 
+// Post new apartment, needs buildingId in params, admin only route!
 export const createApartment = async (req, res, next) => {
     const newApartment = new Apartment(req.body);
     try {
@@ -20,6 +22,9 @@ export const createApartment = async (req, res, next) => {
             { $push: { rooms: savedApartment._id } },
             { new: true }
         );
+        if (!updatedBuilding) {
+            res.status(200).json(`Apartment ${savedApartment.roomNumber} has been created but could not add it to that building!`);
+        }
         res.status(200).json(`Apartment ${savedApartment.roomNumber} has been added to ${updatedBuilding.name}`);
     } catch (err) {
         next(err);
