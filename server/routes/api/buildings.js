be1e0e02e0fa94a2e04bf27f023705f6550fd35b
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-10-02 22:17:34 -0500

    Added comments to routes/api/buildings

--- server/routes/api/buildings.js
+++ server/routes/api/buildings.js
@@ -1,20 +1,21 @@
 import express from "express";
 import { getAllBuildings, getBuilding, createBuilding, updateBuilding, deleteBuilding } from "../../controllers/buildings.js";
+import { userVerify, adminVerify } from "../../utils/verify.js";
 
 const router = express.Router();
 
 // api/buildings
 
-router.get("/", getAllBuildings);
+// Verified user routes || not logged in?
+router.get("/", userVerify, getAllBuildings);
+router.get("/:id", userVerify, getBuilding);
 
-router.get("/:id", getBuilding);
-
-router.post("/", createBuilding);
-
-router.put("/:id", updateBuilding);
+//Admin only routes
+router.post("/", adminVerify, createBuilding);
+router.put("/:id", adminVerify, updateBuilding);
 
 //TODO: Toggle active instead of deleting entirely
-router.delete("/:id", deleteBuilding);
+router.delete("/:id", adminVerify, deleteBuilding);
 
 
 
