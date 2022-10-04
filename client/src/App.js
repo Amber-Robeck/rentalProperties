7942f2d90320a872825f8024f81a38b8e589a507
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-10-03 22:09:25 -0500

    Installed react router dom, created pages for home and login along with routes to both

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Homepage from './pages/home/Homepage.js';
+import Login from './pages/login/Login.js';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Rental properties</h1>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Homepage />} />
+            <Route path="login" element={<Login />} />
+          </Route >
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/home/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <h1>Rental Properties home</h1>
+        </div>
+    )
+}
+
+export default Homepage
