26f095d505a3deb790985b05eca95a2785eceeaa
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-10-04 00:11:16 -0500

    Added basic styles to navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <div className='searchbar'>
+                    <input type="text" placeholder="Search" />
+                </div>
+                <div className='links'>
+                    <div className='link'>ONE</div>
+                    <div className='link'>TWO</div>
+                    <div className='link'>THREE</div>
+                    <div className='link'>FOUR</div>
+                    <div className='link'>FIVE</div>
+                    <div className='link'>SIX</div>
+                </div>
+            </div>
+        </nav>
     )
 }
 
--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Navbar from '../../components/navbar/Navbar.js';
 import SidePanel from '../../components/sidePanel/SidePanel.js';
 
 const Dashboard = () => {
     return (
         <div className='dashboard'>
             <SidePanel />
-            <div className='main-container'>main</div>
+            <div className='main-container'>
+                <Navbar />
+                main
+            </div>
         </div>
     )
 }
