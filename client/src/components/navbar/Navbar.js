18acf15df04be045f3d698150c8eca68e522cd80
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-10-13 21:40:04 -0500

    Added logout functionality to navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from "../../context/UserContext";
 
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+    const { user, dispatch } = useContext(UserContext);
+    const navPage = useNavigate();
+
+    const handleLogout = async (e) => {
+        e.preventDefault()
+        try {
+            dispatch({
+                type: "LOGOUT"
+            });
+            localStorage.removeItem("user")
+            navPage("/")
+        } catch (error) {
+            dispatch({
+                type: "LOGIN_FAILURE",
+                payload: error.response.data
+            });
+        };
+    };
     return (
         <nav className='navbar'>
             <div className='navbar-container'>
@@ -13,10 +33,15 @@ const Navbar = () => {
                     <div className='link'>THREE</div>
                     <div className='link'>FOUR</div>
                     <div className='link'>FIVE</div>
-                    <div className='link'><a href='/login'>login</a></div>
+                    < div className='link' >
+                        {!user?.username
+                            ? <a href='/login'>login</a>
+                            : <a href='/' onClick={(e) => handleLogout(e)}>logout</a>
+                        }
+                    </div>
                 </div>
             </div>
-        </nav>
+        </nav >
     )
 }
 
