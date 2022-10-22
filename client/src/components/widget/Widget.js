f89a67fb9802ea47742d13032bbfb85673d5a236
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-10-21 21:52:31 -0500

    Working on widget display for admin vs user

--- client/src/components/widget/Widget.js
+++ client/src/components/widget/Widget.js
@@ -4,7 +4,7 @@ import { UserContext } from "../../context/UserContext";
 
 const Widget = () => {
     const { user } = useContext(UserContext);
-    console.log(user)
+    console.log(user.isAdmin)
     const userWidget = [
         {
             title: "this thing",
@@ -13,23 +13,55 @@ const Widget = () => {
             info: "something",
             alt: "alt icon",
             icon: ""
+        },
+        {
+            title: "this thing",
+            number: 3,
+            link: "www.google.com",
+            info: "something",
+            alt: "alt icon",
+            icon: ""
         }
-    ]
+    ];
+    const adminWidget = [
+        {
+            title: "this other thing",
+            number: 3,
+            link: "www.google.com",
+            info: "something",
+            alt: "alt icon",
+            icon: ""
+        },
+        {
+            title: "this thing",
+            number: 3,
+            link: "www.google.com",
+            info: "something",
+            alt: "alt icon",
+            icon: ""
+        }
+    ];
     return (
-        <div className="widget">
+        <>
+            {!user.isAdmin ? adminWidget
+                .map((data) => (
+                    <div className="widget" key={data.name}>
+                        <div className='widget-left'>
+                            <p className='widget-title'>{data.title}</p>
+                            <p className='widget-number'>Number of widget</p>
+                            <p className='widget-link'>Link to another page</p>
+                        </div>
+                        <div className='widget-right'>
+                            <p className='widget-info'>Widget info</p>
+                            <p className='widget-alt'>Alt widget</p>
+                            <p className='widget-icon'>Icon</p>
+                        </div>
 
-            <div className='widget-left'>
-                <p className='widget-title'>Title of widget</p>
-                <p className='widget-number'>Number of widget</p>
-                <p className='widget-link'>Link to another page</p>
-            </div>
-            <div className='widget-right'>
-                <p className='widget-info'>Widget info</p>
-                <p className='widget-alt'>Alt widget</p>
-                <p className='widget-icon'>Icon</p>
-            </div>
-        </div>
+                    </div>
+                ))
+                : <p>error things</p>
+            }
+        </>
     )
 }
-
 export default Widget
